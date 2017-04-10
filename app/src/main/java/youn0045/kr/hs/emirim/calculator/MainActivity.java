1b8df4eae703bedd7845c1a2e3be7753c63af6b6
forelise25 <forelise25@naver.com>
2017-04-10 20:57:45 +0900

    #JavaEventProgramming3

--- app/src/main/java/youn0045/kr/hs/emirim/calculator/MainActivity.java
+++ app/src/main/java/youn0045/kr/hs/emirim/calculator/MainActivity.java
@@ -11,19 +11,21 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
     EditText edit1, edit2;
     Button[] butNums=new Button[10];
-    Button[] butOps=new Button[4];
-    public static final int SELECT_EDIT1=0;
-    public static final int SELECT_EDIT2=1;
-    int SelectEdit =SELECT_EDIT1;
-    String numStr="";
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        edit1 = (EditText)findViewById(R.id.edit1); //
+        Button[] butOps=new Button[4];
+        public static final int SELECT_EDIT1=0;
+        public static final int SELECT_EDIT2=1;
+        int SelectEdit =SELECT_EDIT1;
+        String numStr="";
+        TextView textResult;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            edit1 = (EditText)findViewById(R.id.edit1); //
         edit2 = (EditText)findViewById(R.id.edit2);
         edit1.setOnTouchListener(editHandler);
         edit2.setOnTouchListener(editHandler);
+        textResult= (TextView)findViewById(R.id.textresult);
 
         for(int i=0; i<butNums.length;i++){
             butNums[i]=(Button)findViewById(R.id.btn0+i);
@@ -52,7 +54,20 @@ public class MainActivity extends AppCompatActivity {
     View.OnClickListener butOpHandler=new View.OnClickListener(){
         @Override
         public void onClick(View view) {
-            switch()
+            int num1= Integer.parseInt(edit1.getText().toString());
+            int num2= Integer.parseInt(edit1.getText().toString());
+            double result =0;
+            switch(view.getId()){
+                case R.id.btnadd:
+                    result=num1+num2;
+                case R.id.btnminus:
+                    result=num1-num2;
+                case R.id.btnmultiple:
+                    result=num1*num2;
+                case R.id.btndivision:
+                    result= num1/(double)num2;
+            }//endswitch
+            textResult.setText(""+result);
         }
     };
 
